e86f52cb0b22f4b6c9c008ad61c3294585fe9205
Lindsay Qin <lindsaymqin@gmail.com>
2024-02-11 19:30:39 -0800

    entering buildings finally work wooo

--- Assets/Scripts/EnterBuilding.cs
+++ Assets/Scripts/EnterBuilding.cs
@@ -2,15 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.InputSystem;
 
 public class EnterBuilding : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("HEY");
-        if (other.gameObject.tag == "Player"){
+    private bool inBuilding = false;
+
+    public void Update() {
+        
+        if (inBuilding && Input.GetButtonDown("EnterBuilding")) {
             SceneManager.LoadScene("Building1");
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("collided with building");
+        if (other.gameObject.tag == "Player"){
+            inBuilding = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        Debug.Log("exiting building");
+        if (other.gameObject.tag == "Player"){
+            inBuilding = false;
+        }
+    }
+
 }
 
