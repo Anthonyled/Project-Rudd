fc92e84ba22b1c26fc524980f3d9767cc8a6e154
zacharydesoto <zacharysdesoto@gmail.com>
2024-02-23 19:16:30 -0800

    Implement Size-Shifting
    Still need to normalize jump strength to size

--- Assets/Scripts/Player/NewPlayerController.cs
+++ Assets/Scripts/Player/NewPlayerController.cs
@@ -11,15 +11,14 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField] Transform groundCheck;
 
-    Vector2 moveVal;
-    [Header("Movement")]
-    [SerializeField] float moveSpeed = 10f;
-    [SerializeField] float acceleration = 5f;
-    [SerializeField] float deceleration = 10f;
-    [SerializeField] float frictionAmount = 0.2f;
+    private Vector2 moveVal;
+    private float moveSpeed = 10f;
+    private float acceleration = 5f;
+    private float deceleration = 10f;
+    private float frictionAmount = 0.2f;
+    private float jumpHeight = 70f;
 
     [Header("Jump")]
-    [SerializeField] float jumpHeight = 70f;
     [Range(0, 1f)] [SerializeField] float jumpCutMultiplier = 0.5f;
     [SerializeField] float jumpBufferTime = 0.3f;
     [SerializeField] float jumpCoyoteTime = 0.08f;
@@ -28,22 +27,28 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Small")]
     [SerializeField] float smallMass;
+    [SerializeField] Vector2 smallScale;
     [SerializeField] float smallMoveSpeed;
     [SerializeField] float smallAcceleration;
+    [SerializeField] float smallDeceleration;
     [SerializeField] float smallJumpHeight;
     [SerializeField] float smallGravity;
 
     [Header("Medium")]
     [SerializeField] float mediumMass;
+    [SerializeField] Vector2 mediumScale;
     [SerializeField] float mediumMoveSpeed;
     [SerializeField] float mediumAcceleration;
+    [SerializeField] float mediumDeceleration;
     [SerializeField] float mediumJumpHeight;
     [SerializeField] float mediumGravity;
 
     [Header("Big")]
     [SerializeField] float bigMass;
+    [SerializeField] Vector2 bigScale;
     [SerializeField] float bigMoveSpeed;
     [SerializeField] float bigAcceleration;
+    [SerializeField] float bigDeceleration;
     [SerializeField] float bigJumpHeight;
     [SerializeField] float bigGravity;
 
@@ -65,18 +70,20 @@ public class PlayerMovement : MonoBehaviour
     // Sizeshifting variables
     [SerializeField] float transformationTime;
 
-    private int curSize = Medium;
+    private size curSize;
     private bool canScale = true;
-    enum size
+    public enum size
     {
         Small,
         Medium,
         Big
-    }
+    };
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        
     }
 
     private void Start()
@@ -86,6 +93,15 @@ public class PlayerMovement : MonoBehaviour
             isFacingRight = false;
         }
         else {isFacingRight = true;}
+
+        curSize = size.Medium;
+        rb.mass = mediumMass;
+        transform.localScale = mediumScale;
+        moveSpeed = mediumMoveSpeed;
+        acceleration = mediumAcceleration;
+        deceleration = mediumDeceleration;
+        jumpHeight = mediumJumpHeight;
+        rb.gravityScale = mediumGravity;
 
         controlsActive = true;
     }
@@ -223,7 +239,6 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         bool grounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, LayerMask.GetMask("Ground"));
-        Debug.Log(grounded ? "grounded" : "not grounded");
         return grounded;
     }
 
@@ -296,7 +311,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (context.performed)
         {
-            if (curSize != Big && canScale)
+            if (curSize != size.Big && canScale)
             {
                 ChangeBigger();
             }
@@ -307,7 +322,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (context.performed)
         {
-            if (curSize != Small && canScale)
+            if (curSize != size.Small && canScale)
             {
                 ChangeSmaller();
             }
@@ -319,12 +334,16 @@ public class PlayerMovement : MonoBehaviour
         canScale = false;
 
         size endSize;
-        if (curSize == Small) {
-            endSize = Medium;
+        if (curSize == size.Small) {
+            endSize = size.Medium;
         }
-        else if (curSize == Medium) {
-            endSize = Big;
-        } // curSize shouldn't ever be Big
+        else if (curSize == size.Medium) {
+            endSize = size.Big;
+        } 
+        else { // curSize shouldn't ever be Big
+            Debug.Log("ERROR: called ChangeBigger when big.");
+            endSize = size.Big;
+        }
 
         StartCoroutine(ScaleAnimation(curSize, endSize));
         curSize = endSize; // May need to change later
@@ -336,12 +355,16 @@ public class PlayerMovement : MonoBehaviour
         canScale = false;
 
         size endSize;
-        if (curSize == Big) {
-            endSize = Medium;
+        if (curSize == size.Big) {
+            endSize = size.Medium;
         }
-        else if (curSize == Medium) {
-            endSize = Small;
-        } // curSize shouldn't ever be Small
+        else if (curSize == size.Medium) {
+            endSize = size.Small;
+        } 
+        else { // curSize shouldn't ever be Small
+            Debug.Log("ERROR: called ChangeSmaller when small.");
+            endSize = size.Small;
+        }
 
         StartCoroutine(ScaleAnimation(curSize, endSize));
         curSize = endSize; // May need to change later
@@ -355,60 +378,76 @@ public class PlayerMovement : MonoBehaviour
         float rate = 1 / transformationTime;
         
         // Feel free to clean this section up if there's a better way to store these values
-        if (startSize == Small)
+        float startMass = rb.mass;
+        Vector2 startScale = transform.localScale;
+        startScale.x = Mathf.Abs(startScale.x);
+
+        float startMovespeed = moveSpeed;
+        float startAcceleration = acceleration;
+        float startDeceleration = deceleration;
+        float startJumpHeight = jumpHeight;
+        float startGravity = rb.gravityScale;
+
+        float endMass = 0;
+        Vector2 endScale = Vector2.zero;
+        float endMovespeed = 0;
+        float endAcceleration = 0;
+        float endDeceleration = 0;
+        float endJumpHeight = 0;
+        float endGravity = 0;
+
+        if (endSize == size.Small)
         {
-            float startMass = smallMass;
-            float startMovespeed = smallMoveSpeed;
-            float startAcceleration = smallAcceleration;
-            float startJumpHeight = smallJumpHeight;
-            float startGravity = smallGravity;
-        }
-        if (startSize == Medium)
-        {
-            float startMass = mediumMass;
-            float startMovespeed = mediumMoveSpeed;
-            float startAcceleration = mediumAcceleration;
-            float startJumpHeight = mediumJumpHeight;
-            float startGravity = mediumGravity;
-        }
-        if (startSize == Big)
-        {
-            float startMass = bigMass;
-            float startMovespeed = bigMoveSpeed;
-            float startAcceleration = bigAcceleration;
-            float startJumpHeight = bigJumpHeight;
-            float startGravity = bigGravity;
+            endMass = smallMass;
+            endScale = smallScale;
+            endMovespeed = smallMoveSpeed;
+            endAcceleration = smallAcceleration;
+            endDeceleration = smallDeceleration;
+            endJumpHeight = smallJumpHeight;
+            endGravity = smallGravity;
         }
 
-        if (endSize == Big)
+        if (endSize == size.Medium)
         {
-            float endMass = bigMass;
-            float endMovespeed = bigMoveSpeed;
-            float endAcceleration = bigAcceleration;
-            float endJumpHeight = bigJumpHeight;
-            float endGravity = bigGravity;
+            endMass = mediumMass;
+            endScale = mediumScale;
+            endMovespeed = mediumMoveSpeed;
+            endAcceleration = mediumAcceleration;
+            endDeceleration = mediumDeceleration;
+            endJumpHeight = mediumJumpHeight;
+            endGravity = mediumGravity;
         }
 
-        Vector2 fromScale = transform.localScale;
-        Vector2 toScale = transform.localScale * scale;
-
-        float startingMass = rb.mass;
-        float startingGravityScale = rb.gravityScale;
+        if (endSize == size.Big)
+        {
+            endMass = bigMass;
+            endScale = bigScale;
+            endMovespeed = bigMoveSpeed;
+            endAcceleration = bigAcceleration;
+            endDeceleration = bigDeceleration;
+            endJumpHeight = bigJumpHeight;
+            endGravity = bigGravity;
+        }
+        
         while (i < 1)
         {
             i += Time.deltaTime * rate; // i is on a scale from 0 to 1, with 0 being the start of the animation and 1 being the end
-            Vector2 p = rb.mass * rb.velocity; // What does this do? -Zach
+            // Vector2 p = rb.mass * rb.velocity; //fixme
             
-            Vector2 newScale = Vector2.Lerp(fromScale, toScale, i); // Lerp does a linear scale from the start to end
-            if (!isFacingRight)
-            {
-                newScale.x *= -1; // Face left
-            }
-            transform.localScale = newScale;
-            rb.mass = Mathf.Lerp(startingMass, startingMass * scale, i);
-            rb.velocity = p / rb.mass; // What does this do? -Zach
+            rb.mass = Mathf.Lerp(startMass, endMass, i); // Lerp does a linear scale from the start to end
+            moveSpeed = Mathf.Lerp(startMovespeed, endMovespeed, i);
+            acceleration = Mathf.Lerp(startAcceleration, endAcceleration, i);
+            deceleration = Mathf.Lerp(startDeceleration, endDeceleration, i);
+            jumpHeight = Mathf.Lerp(startJumpHeight, endJumpHeight, i);
+            rb.gravityScale = Mathf.Lerp(startMass, endMass, i);
 
-            rb.gravityScale = Mathf.Lerp(startingGravityScale, startingGravityScale * scale, i); // Scale gravity
+            Vector2 newScale = Vector2.Lerp(startScale, endScale, i); 
+
+            newScale.x = Mathf.Abs(newScale.x) * (isFacingRight ? 1 : -1);
+    
+            transform.localScale = newScale;
+            // rb.velocity = p / rb.mass; // 
+
             yield return 0;
         }
     }
