0d13281fa9cd7d2f3842a9821f47e753eb08ee45
Anthonyled <130782897+Anthonyled@users.noreply.github.com>
2024-05-13 21:47:48 -0700

    Update PlayerController.cs
    Changing the respawn Coordinates

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -161,7 +161,7 @@ public class PlayerController : MonoBehaviour
             FallThroughPlatforms();
         }
 
-        if (rb.position.y < -10)
+        if (rb.position.y < -100)
         {
             rb.position = respawnCoords;
             rb.velocity = Vector2.zero;
