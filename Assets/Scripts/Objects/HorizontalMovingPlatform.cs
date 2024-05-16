f4d329b0727c4ee7ce595488f4dbae033160b290
zacharydesoto <zacharysdesoto@gmail.com>
2024-05-16 15:29:07 -0700

    Implement Horizontal Moving Platform
    Made platform that moves horizontally, still need to make it so it carries player with it.

---
+++ Assets/Scripts/Objects/HorizontalMovingPlatform.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovingPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    [SerializeField] GameObject pointA;
+    [SerializeField] GameObject pointB;
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(speed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < pointA.transform.position.x)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        if (transform.position.x > pointB.transform.position.x)
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+    }
+
+}
